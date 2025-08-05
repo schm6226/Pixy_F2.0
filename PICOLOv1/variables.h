f986437654e87e2f8f7916316471d4df5c614b27
Henry2374 <gordo883@umn.edu>
2025-08-05 15:41:14 -0500

    Update variables.h
    Signed-off-by: Henry2374 <gordo883@umn.edu>

--- PICOLOv1/variables.h
+++ PICOLOv1/variables.h
@@ -193,7 +193,7 @@ float errorZ = 0;
 const int tiltPin = 7;
 int tiltVal = 0;
 String tiltMode = "Idle";
-int maxVal = 0; // smallest possible Value
+int maxVal = 0; // smallest possible Value make sure these are correct
 int minVal = 1023; //largest possible Value
 // //pitch
 // float ax = accelerometer[0]; // X-axis acceleration
