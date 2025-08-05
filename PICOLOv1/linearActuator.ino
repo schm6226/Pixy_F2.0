0772d39f2ac077542ab57261dd9e26796adfec2e
Henry2374 <gordo883@umn.edu>
2025-08-05 13:42:02 -0500

    Update linearActuator.ino
    uncommenting everything to worko nin my branch
    
    Signed-off-by: Henry2374 <gordo883@umn.edu>

--- PICOLOv1/linearActuator.ino
+++ PICOLOv1/linearActuator.ino
@@ -1,111 +1,111 @@
-// /*
-// _____________________________________________________________
-// Code for the reaction control system controlled by a Linear Actuator
-// Using SparkFun TB6612FNG driver and BNO055 IMU
-// Code by: Andrew Schmit
-// Last modified: 6/30/2025
-// _____________________________________________________________
-// */
+/*
+_____________________________________________________________
+Code for the reaction control system controlled by a Linear Actuator
+Using SparkFun TB6612FNG driver and BNO055 IMU
+Code by: Andrew Schmit
+Last modified: 6/30/2025
+_____________________________________________________________
+*/
 
-// void Actuatorsetup() {
+void Actuatorsetup() {
 
-//   analogWrite(PWMB, moveDuty); 
+  analogWrite(PWMB, moveDuty); 
 
-//   printOLED("Starting Actuator test.");
+  printOLED("Starting Actuator test.");
   
-//   moveActuator(false);
-//   for (int i = 0; i < 50; i++){
-//     delay(100);
-//     pos = analogRead(feedbackPin);
-//     printOLED(String(pos));
-//   }
-//   maxPos = pos;
+  moveActuator(false);
+  for (int i = 0; i < 50; i++){
+    delay(100);
+    pos = analogRead(feedbackPin);
+    printOLED(String(pos));
+  }
+  maxPos = pos;
 
-//   moveActuator(true);
-//   for (int i = 0; i < 50; i++){
-//     delay(100);
-//     pos = analogRead(feedbackPin);
-//     printOLED(String(pos));
-//   }
-//   minPos = pos;
+  moveActuator(true);
+  for (int i = 0; i < 50; i++){
+    delay(100);
+    pos = analogRead(feedbackPin);
+    printOLED(String(pos));
+  }
+  minPos = pos;
   
-//   moveActuator(false);
-//   for (int i = 0; i < 50; i++){
-//     delay(100);
-//     pos = analogRead(feedbackPin);
-//     truePos = posMap(pos,minPos,maxPos);
-//     printOLED(String(truePos));
-//   }
+  moveActuator(false);
+  for (int i = 0; i < 50; i++){
+    delay(100);
+    pos = analogRead(feedbackPin);
+    truePos = posMap(pos,minPos,maxPos);
+    printOLED(String(truePos));
+  }
   
-//   resetActuator();
-//   printOLED(String(truePos));
+  resetActuator();
+  printOLED(String(truePos));
 
-//   printOLED("Actuator test complete.");
-// }
+  printOLED("Actuator test complete.");
+}
 
-// float posMap(int pos,int min,int max){
-//   return (float)(pos - min) * (5.0 / (max - min)); // only change that might be meaningful
-// }
-// // Moves actuator in a direction at duty cycle
-// // Stops the actuator
-// void stopActuator() {
-//   analogWrite(PWMB, 0);
-// }
+float posMap(int pos,int min,int max){
+  return (float)(pos - min) * (5.0 / (max - min)); // only change that might be meaningful
+}
+// Moves actuator in a direction at duty cycle
+// Stops the actuator
+void stopActuator() {
+  analogWrite(PWMB, 0);
+}
 
-// void resetActuator() {
-//   // Continuously move actuator until it's within the desired range
-//   while (true) {
-//     pos = analogRead(feedbackPin);
-//     truePos = posMap(pos, minPos, maxPos);
+void resetActuator() {
+  // Continuously move actuator until it's within the desired range
+  while (true) {
+    pos = analogRead(feedbackPin);
+    truePos = posMap(pos, minPos, maxPos);
 
-//     if (truePos > targetPos + tolerance) {
-//       moveActuator(true);   // Move toward retracted
-//     }
-//     else if (truePos < targetPos - tolerance) {
-//       moveActuator(false);  // Move toward extended
-//     }
-//     else {
-//       stopActuator(); 
-//       delay(100);
-//       Serial.println("centered");
-//       break;  // Exit loop when within range
-//     }
-//     delay(50);  // Give time for motor to move
-//   }
-// }
+    if (truePos > targetPos + tolerance) {
+      moveActuator(true);   // Move toward retracted
+    }
+    else if (truePos < targetPos - tolerance) {
+      moveActuator(false);  // Move toward extended
+    }
+    else {
+      stopActuator(); 
+      delay(100);
+      Serial.println("centered");
+      break;  // Exit loop when within range
+    }
+    delay(50);  // Give time for motor to move
+  }
+}
 
-// void updateLinearActuator() {  
-//   if(pixy.ccc.numBlocks > 0){
-//      proportionalZ = KPZ * errorZ; // find proportional term
-//      Serial.println("linear Actuator stuff: " + String(proportionalZ) + ", " + String(errorZ) + ", " + String(tiltOffset));
-//       if (proportionalZ < -5){
-//         Serial.println("move true");
-//         moveActuator(true);
-//       }
-//       else if (proportionalZ > 5) {
-//         Serial.println("move false");
-//         moveActuator(false);
-//       }
-//       else {
-//         Serial.println("move stop");
-//         stopActuator();
-//       }
-//     }
-//   else{
-//     stopActuator();
-//   }
-// }
-// // Reads and prints position every 100 ms for `duration` milliseconds
-// void moveActuator(bool extend) {
-//   digitalWrite(BIN1, extend ? HIGH : LOW);
-//   digitalWrite(BIN2, extend ? LOW : HIGH);
-//   analogWrite(PWMB, moveDuty);  // 20% 
-//   Serial.println("movibg");
-// }
+void updateLinearActuator() {  
+  if(pixy.ccc.numBlocks > 0){
+     proportionalZ = KPZ * errorZ; // find proportional term
+     Serial.println("linear Actuator stuff: " + String(proportionalZ) + ", " + String(errorZ) + ", " + String(tiltOffset));
+      if (proportionalZ < -5){
+        Serial.println("move true");
+        moveActuator(true);
+      }
+      else if (proportionalZ > 5) {
+        Serial.println("move false");
+        moveActuator(false);
+      }
+      else {
+        Serial.println("move stop");
+        stopActuator();
+      }
+    }
+  else{
+    stopActuator();
+  }
+}
+// Reads and prints position every 100 ms for `duration` milliseconds
+void moveActuator(bool extend) {
+  digitalWrite(BIN1, extend ? HIGH : LOW);
+  digitalWrite(BIN2, extend ? LOW : HIGH);
+  analogWrite(PWMB, moveDuty);  // 20% 
+  Serial.println("movibg");
+}
 
-// // float calculatePitch(float pitch){
-// //   sensors_event_t event;
-// //   bno.getEvent(&event);
-// //   pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / PI;
-// //   return pitch;
-// // }
+// float calculatePitch(float pitch){
+//   sensors_event_t event;
+//   bno.getEvent(&event);
+//   pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / PI;
+//   return pitch;
+// }
